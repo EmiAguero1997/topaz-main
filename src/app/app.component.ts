5df41e5fa353877d8ffe1b240a05f601ab34febf
Emiliano Aguero <e.aguero@birthstones.app>
2025-03-22 20:39:16 -0300

    advances with navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule, AvatarModule, AvatarGroupModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'topaz-main';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Areas',
+              
+          },
+          {
+              label: 'Locaciones',
+            
+          },
+          {
+            label: 'Proveedores',
+           
+        },
+        {
+            label: 'Pedidos',
+            
+        },
+        {
+            label: 'Ordenes de compra',
+            
+        },
+      ];
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import { MyPreset } from './themes/perfil-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
+            preset: MyPreset
         }
     })]
 };
