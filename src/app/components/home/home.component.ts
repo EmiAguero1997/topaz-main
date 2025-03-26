3bfa4433d37d0124ecd182ad4b70a8fe276eb9e8
Emiliano Aguero <e.aguero@birthstones.app>
2025-03-26 16:41:21 -0300

    get identity

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,11 +10,16 @@ import { ButtonModule } from 'primeng/button';
 })
 export class HomeComponent {
   token!:string;
+  identity:any;
   constructor(private ar:ActivatedRoute){
     this.ar.queryParams.subscribe(params=>{
       this.token = params['token'];
+      let identity = params['identity'];
+      this.identity = JSON.parse(identity);
       sessionStorage.setItem('token', this.token);
+      sessionStorage.setItem('identity', this.identity);
       console.log('token: ', this.token);
+      console.log('identity: ', this.identity);
     })
     
   }
